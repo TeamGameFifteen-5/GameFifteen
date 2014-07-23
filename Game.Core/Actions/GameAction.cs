e0be8ba0c828edbea3571c1ecd55c0852dcbf702
ventsislav-georgiev <ventsislav.georgiev@live.com>
2014-07-23 23:31:06 +0300

    Missing file

---
+++ Game.Core/Actions/GameAction.cs
@@ -0,0 +1,32 @@
+﻿using Game.Core.Actions.ActionInvokers;
+
+namespace Game.Core.Actions
+{
+	public abstract class GameAction : IGameAction
+	{
+		public GameAction(ActionType actionType, IActionInvoker actionInvoker)
+		{
+			this.ActionInvoker = actionInvoker;
+			this.ActionType = actionType;
+		}
+
+		protected IActionInvoker ActionInvoker;
+		protected ActionType ActionType;
+
+		public virtual void Execute()
+		{
+			this.ActionInvoker.Invoke(this.ActionType);
+		}
+
+		public virtual void UnExecute()
+		{
+			var undoActionType = this.GetUndoActionType(this.ActionType);
+			this.ActionInvoker.Invoke(undoActionType);
+		}
+
+		protected virtual ActionType GetUndoActionType(ActionType actionType)
+		{
+			return ActionType.Unmapped;
+		}
+	}
+}
