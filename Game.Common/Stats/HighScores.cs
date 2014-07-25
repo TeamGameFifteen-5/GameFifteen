543c72de1de30ff25e47b96c49ddf40fcd84ef7b
andrei-pl <andrey.pld@gmail.com>
2014-07-25 23:14:27 +0300

    Serialize and Deserialize results
    - Save and Load results from file added

--- Game.Common/Stats/HighScores.cs
+++ Game.Common/Stats/HighScores.cs
@@ -1,54 +1,103 @@
-﻿using Game.Common.Players;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace Game.Common.Stats
+﻿namespace Game.Common.Stats
 {
-	public sealed class HighScores : IHighScores
-	{
-		private const int MAX_TOP_PLAYERS = 5;
-		private static readonly IHighScores instance = new HighScores();
+    using Game.Common.Players;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Runtime.Serialization.Formatters.Binary;
 
-		private List<IPlayer> highScores;
+    public sealed class HighScores : IHighScores
+    {
+        private const int MaxTopPlayers = 5;
+        private const string fileAddress = @"../../GameFifteen.game15";
+        private static readonly IHighScores instance = new HighScores();
 
-		private HighScores()
-		{
-			this.highScores = new List<IPlayer>(MAX_TOP_PLAYERS);
-		}
+        private List<IPlayer> highScores;
 
-		public static IHighScores Instance
-		{
-			get
-			{
-				return instance;
-			}
-		}
+        private HighScores()
+        {
+            this.highScores = this.LoadFromFile();
 
-		public List<IPlayer> Load()
-		{
-			return highScores;
-		}
+            if (this.highScores == null)
+            {
+                this.highScores = new List<IPlayer>(MaxTopPlayers);
+            }
+        }
 
-		public void Save(IPlayer player)
-		{
-			if (highScores.Count < MAX_TOP_PLAYERS)
-			{
-				highScores.Add(new Player(player));
-			}
-			else
-			{
-				foreach (var person in highScores)
-				{
-					if (player.Score <= person.Score)
-					{
-						highScores.Remove(highScores[MAX_TOP_PLAYERS - 1]);
-						highScores.Add(new Player(player));
-						break;
-					}
-				}
-			}
+        public static IHighScores Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
 
-			highScores = highScores.OrderBy(x => x.Score).ToList();
-		}
-	}
+        public List<IPlayer> Load()
+        {
+            return this.highScores;
+        }
+
+        public void Save(IPlayer player)
+        {
+            if (this.highScores.Count < MaxTopPlayers)
+            {
+                this.highScores.Add(new Player(player));
+            }
+            else
+            {
+                foreach (var person in highScores)
+                {
+                    if (player.Score <= person.Score)
+                    {
+                        this.highScores.Remove(this.highScores[MaxTopPlayers - 1]);
+                        this.highScores.Add(new Player(player));
+                        break;
+                    }
+                }
+            }
+
+            this.Sort();
+            this.SaveInFile();
+        }
+
+        public void Sort()
+        {
+            highScores = highScores.OrderBy(x => x.Score).ToList();
+        }
+
+        private void SaveInFile()
+        {
+            using (Stream file = File.Open(fileAddress, FileMode.OpenOrCreate))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(file, this.highScores);
+            }
+        }
+
+        private List<IPlayer> LoadFromFile()
+        {
+            Stream stream = null;
+            List<IPlayer> highScoresFromFile;
+            try
+            {
+                using (stream = File.Open(fileAddress, FileMode.OpenOrCreate))
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    highScoresFromFile = (List<IPlayer>)formatter.Deserialize(stream);
+                }
+            }
+            catch (Exception)
+            {
+                if (stream != null)
+                {
+                    stream.Close();
+                }
+
+                return null;
+            }
+
+            return highScoresFromFile;
+        }
+    }
 }
