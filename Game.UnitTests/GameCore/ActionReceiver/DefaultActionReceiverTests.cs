afc1a79d9d0b1bf88338177e3a1b43bc17dfaa6a
nikolay-radkov <nikolay.kirilov.radkov@gmail.com>
2014-07-27 14:35:36 +0300

    Added unit tests for ActionReceiver.cs

---
+++ Game.UnitTests/GameCore/ActionReceiver/DefaultActionReceiverTests.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Game.Core.Actions.ActionReceiver;
+using Game.UnitTests.GameCore.SampleGameEngine;
+using Game.Common;
+using System.IO;
+using Game.Common.Stats;
+using System.Text;
+using Game.Common.Players;
+using Game.Core;
+using Game.Common.Map.Movement;
+
+namespace Game.UnitTests.GameCore.ActionReceiver
+{
+    [TestClass]
+    public class DefaultActionReceiverTests
+    {
+        private readonly DefaultActionReceiver receiver = new DefaultActionReceiver(FakeGameEngine.Engine);
+        private readonly IPlayer player = FakeGameEngine.Player;
+        private FileStream ostrm;
+        private StreamWriter writer;
+        private readonly TextWriter oldOut = Console.Out;
+        private StreamReader reader;
+        private readonly string filePath = "../../console-output.game15";
+
+        private void ChangeConsoleOutPut()
+        {
+            try
+            {
+                ostrm = new FileStream(filePath, FileMode.Truncate, FileAccess.Write);
+                writer = new StreamWriter(ostrm);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot open console-output.txt for writing");
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            Console.SetOut(writer);
+        }
+
+        private void ReverseConsoleOutPut()
+        {
+            Console.SetOut(oldOut);
+            writer.Close();
+            ostrm.Close();
+        }
+
+        [TestMethod]
+        public void IsValidExecuteWithUnmappedActionTypeNameTest()
+        {
+            ChangeConsoleOutPut();
+
+            var actionType = new ActionType("Unmapped");
+            receiver.Execute(actionType);
+
+            ReverseConsoleOutPut();
+
+            using (reader = new StreamReader(filePath))
+            {
+                string result = reader.ReadToEnd();
+
+                Assert.AreEqual(result, "Illegal move!" + Environment.NewLine);
+            }
+        }
+
+        [TestMethod]
+        public void IsValidExecuteWithExitActionTypeNameTest()
+        {
+            ChangeConsoleOutPut();
+
+            var actionType = new ActionType("Exit");
+            receiver.Execute(actionType);
+
+            ReverseConsoleOutPut();
+
+            using (reader = new StreamReader(filePath))
+            {
+                string result = reader.ReadToEnd();
+
+                Assert.AreEqual(result, "Good bye!" + Environment.NewLine);
+            }
+        }
+
+        [TestMethod]
+        public void IsValidExecuteWithIllegalCommandActionTypeNameTest()
+        {
+            ChangeConsoleOutPut();
+
+            var actionType = new ActionType("IlligalCommand");
+            receiver.Execute(actionType);
+
+            ReverseConsoleOutPut();
+
+            using (reader = new StreamReader(filePath))
+            {
+                string result = reader.ReadToEnd();
+
+                Assert.AreEqual(result, "Illegal command!" + Environment.NewLine);
+            }
+        }
+
+        [TestMethod]
+        public void IsValidExecuteWithScoresActionTypeNameTest()
+        {
+            ChangeConsoleOutPut();
+
+            var actionType = new ActionType("Scores");
+            receiver.Execute(actionType);
+
+            ReverseConsoleOutPut();
+
+            var UP_DOWN_TABLE_FRAME = "-------------------------";
+            var stats = InMemoryScores.Instance;
+            var playerScores = stats.Load();
+
+            var expectedResult = new StringBuilder();
+
+            expectedResult.AppendLine(UP_DOWN_TABLE_FRAME);
+
+            foreach (var playerScore in playerScores)
+            {
+                expectedResult.AppendFormat("{0}: {1}{2}", playerScore.Name, playerScore.Value, Environment.NewLine);
+            }
+
+            expectedResult.AppendLine(UP_DOWN_TABLE_FRAME);
+
+            using (reader = new StreamReader(filePath))
+            {
+                string result = reader.ReadToEnd();
+
+                Assert.AreEqual(result, expectedResult.ToString());
+            }
+        }
+
+        [TestMethod]
+        public void IsValidIncrementsPlayerScoreTest()
+        {
+            // Assigned field position and player and player score
+            FakeGameEngine.Engine.StartGame();
+
+            var playerScoreBeforeAction = player.Score;
+            var actionType = new ActionType("Down");
+            receiver.Execute(actionType);
+
+            var playerScoreAfterAction = player.Score;
+            playerScoreBeforeAction++;
+
+            Assert.AreEqual(playerScoreBeforeAction, playerScoreAfterAction);
+        }
+
+        [TestMethod]
+        public void IsValidDirectionChangeTest()
+        {
+            // Assigned field position and player and player score
+            FakeGameEngine.Engine.StartGame();
+        
+            var movement = FakeGameEngine.Movement as BackwardMovement;
+
+            if (movement != null)
+            {
+                IsValidBackwardMovementTest();
+            }
+            else
+            {
+                IsValidStraightMovementTest();
+            }
+        }
+
+        [Ignore]
+        private void IsValidBackwardMovementTest()
+        {
+            var startPositionX = FakeGameEngine.Field.Position.X;
+            var startPositionY = FakeGameEngine.Field.Position.Y;
+
+            receiver.Execute(new ActionType("Right"));
+            Assert.AreEqual(startPositionY, FakeGameEngine.Field.Position.Y);
+            Assert.AreEqual(startPositionX - 1, FakeGameEngine.Field.Position.X);
+
+            receiver.Execute(new ActionType("Down"));
+            Assert.AreEqual(startPositionY - 1, FakeGameEngine.Field.Position.Y);
+            Assert.AreEqual(startPositionX - 1, FakeGameEngine.Field.Position.X);
+
+            receiver.Execute(new ActionType("Left"));
+            Assert.AreEqual(startPositionY - 1, FakeGameEngine.Field.Position.Y);
+            Assert.AreEqual(startPositionX, FakeGameEngine.Field.Position.X);
+
+            receiver.Execute(new ActionType("Up"));
+            Assert.AreEqual(startPositionY, FakeGameEngine.Field.Position.Y);
+            Assert.AreEqual(startPositionX, FakeGameEngine.Field.Position.X);
+        }
+
+        [Ignore]
+        private void IsValidStraightMovementTest()
+        {
+            var startPositionX = FakeGameEngine.Field.Position.X;
+            var startPositionY = FakeGameEngine.Field.Position.Y;
+
+            receiver.Execute(new ActionType("Right"));
+            Assert.AreEqual(startPositionY, FakeGameEngine.Field.Position.Y);
+            Assert.AreEqual(startPositionX + 1, FakeGameEngine.Field.Position.X);
+
+            receiver.Execute(new ActionType("Down"));
+            Assert.AreEqual(startPositionY + 1, FakeGameEngine.Field.Position.Y);
+            Assert.AreEqual(startPositionX + 1, FakeGameEngine.Field.Position.X);
+
+            receiver.Execute(new ActionType("Left"));
+            Assert.AreEqual(startPositionY + 1, FakeGameEngine.Field.Position.Y);
+            Assert.AreEqual(startPositionX, FakeGameEngine.Field.Position.X);
+
+            receiver.Execute(new ActionType("Up"));
+            Assert.AreEqual(startPositionY, FakeGameEngine.Field.Position.Y);
+            Assert.AreEqual(startPositionX, FakeGameEngine.Field.Position.X);
+
+        }
+    }
+}
