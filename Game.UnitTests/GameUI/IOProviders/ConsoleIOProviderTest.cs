fbe5e3b8c24d18d3d893743a6b26c70256d0cb26
ventsislav-georgiev <ventsislav.georgiev@live.com>
2014-07-27 21:55:20 +0300

    Addeed more IOProvider tests

--- Game.UnitTests/GameUI/IOProviders/ConsoleIOProviderTest.cs
+++ Game.UnitTests/GameUI/IOProviders/ConsoleIOProviderTest.cs
@@ -1,9 +1,11 @@
 ﻿namespace Game.UnitTests.GameUI.IOProviders
 {
 	using Game.Common;
+	using Game.UI.IOProviders.Settings;
 	using Game.UI.Windows.Console.IOProviders;
 	using Microsoft.VisualStudio.TestTools.UnitTesting;
 	using System.Diagnostics.CodeAnalysis;
+	using System.Drawing;
 
 	[TestClass]
 	[ExcludeFromCodeCoverage]
@@ -13,6 +15,9 @@
 		public void Map()
 		{
 			var ioProvider = new ConsoleIOProvider();
+			var ioProviderSettings = new DefaultIOProviderSettings(Color.White);
+			ioProvider.ApplySettings(ioProviderSettings);
+
 			var consoleKey = new System.ConsoleKeyInfo('W', System.ConsoleKey.W, false, false, false);
 			var actionType = ioProvider.Map(consoleKey);
 
