e0401da3a8edc39fef4326ef48ddaf4784e43f1c
ventsislav-georgiev <ventsislav.georgiev@live.com>
2014-07-27 21:51:14 +0300

    Added ConsoleIOProvider Map Test

---
+++ Game.UnitTests/GameUI/IOProviders/ConsoleIOProviderTest.cs
@@ -0,0 +1,22 @@
+﻿namespace Game.UnitTests.GameUI.IOProviders
+{
+	using Game.Common;
+	using Game.UI.Windows.Console.IOProviders;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using System.Diagnostics.CodeAnalysis;
+
+	[TestClass]
+	[ExcludeFromCodeCoverage]
+	public class ConsoleIOProviderTest
+	{
+		[TestMethod]
+		public void Map()
+		{
+			var ioProvider = new ConsoleIOProvider();
+			var consoleKey = new System.ConsoleKeyInfo('W', System.ConsoleKey.W, false, false, false);
+			var actionType = ioProvider.Map(consoleKey);
+
+			Assert.AreEqual(actionType, ActionType.Get(DefaultActionTypes.Up));
+		}
+	}
+}
